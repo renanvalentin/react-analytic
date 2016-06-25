eec8148f39ff04bedf892db10be65da2cd4db5b7
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-06-25 15:39:56 -0400

    get modules from file

--- src/file_parser.js
+++ src/file_parser.js
@@ -1,7 +1,26 @@
 module.exports = {
-  extractModule: (modulePath) => {
-   const result = modulePath.match(/(?:import)\s(.+)(?:from)\s['|"](.*)['|"];?/)
-   return result.pop();
-  }
+  parse,
+  extractModule
 }
+
+function parse(file) {
+  return file.split('\n').reduce((modules, line) => {
+    const module = extractModule(line)
+
+    if(module)
+      modules.push(module)
+
+    return modules
+  }, [])
+}
+
+function extractModule(modulePath) {
+ const result = modulePath.match(/(?:import)\s(.+)(?:from)\s['|"](.*)['|"];?/)
+
+  if(!result) return false
+
+  return result.pop()
+}
+
+
 
---
+++ test/file_parser.js
@@ -0,0 +1,27 @@
+const fileParser = require('../src/file_parser.js')
+
+describe('File Parser', () => {
+  it.only('should get a list of modules from the file', () => {
+    const file = [
+      'import ModuleA from \'path/to/ModuleA\'',
+      'import ModuleA from \'path/to/ModuleB\'',
+      'import ModuleA from \'path/to/ModuleC\'',
+      '',
+      '// some js...'
+    ].join('\n')
+
+    const modules = fileParser.parse(file)
+    modules.should.eql([
+      'path/to/ModuleA',
+      'path/to/ModuleB',
+      'path/to/ModuleC'
+    ])
+  })
+
+  it('should extract the path from the imported module', () => {
+     const module = 'import Component from \'path/to/Component\''
+     const path = fileParser.extractModule(module)
+
+     path.should.equal('path/to/Component')
+  })
+})
