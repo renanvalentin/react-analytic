be6b9ad603000f901bd6bdb6236e83581e494813
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-06-28 20:29:42 -0400

    webserver setup

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  browserify = require('browserify'),
+  source = require('vinyl-source-stream');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['app', 'tmp'],
+    livereload: true
+  });
+});
+
+gulp.task('browserify', function() {
+    return browserify('./app/main.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./tmp/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/*.js'], ['browserify']);
+});
+
+gulp.task('default', ['connect', 'watch']);
