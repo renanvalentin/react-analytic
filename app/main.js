e947054e8d4527f3ecbaf441098b42841bcf1f4d
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-07-01 19:40:28 -0400

    Load data

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
-alert('hello wolrd!')
+const graph = require('./graph')
+
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'data.json')
+xhr.onload = () => {
+  const graphic = graph.create(JSON.parse(xhr.response))
+  console.log(graphic)
+}
+
+xhr.send()
+
+function setup() {
+
+}
+
+function draw() {
+  ellipse(50, 50, 50, 50)
+}
--- tmp/bundle.js
+++ tmp/bundle.js
@@ -1,4 +1,49 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-alert('hello wolrd!')
+module.exports = {
+  create
+}
 
-},{}]},{},[1]);
+function create(state) {
+ return Object.keys(state).reduce((tree, key) => createCoords(state, key, tree), {})
+}
+
+function createCoords(state, key, tree) {
+  const node = state[key]
+
+  const leaves = Object.keys(tree).length
+
+  const minimumSize = 10
+  const size = minimumSize + node.references.length
+
+  tree[key] = {
+    x: leaves * size,
+    y: leaves * size,
+    size: size,
+    dependencies: node.dependencies.slice(),
+    references: node.references.slice()
+  }
+
+  return tree
+}
+
+},{}],2:[function(require,module,exports){
+const graph = require('./graph')
+
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'data.json')
+xhr.onload = () => {
+  const graphic = graph.create(JSON.parse(xhr.response))
+  console.log(graphic)
+}
+
+xhr.send()
+
+function setup() {
+
+}
+
+function draw() {
+  ellipse(50, 50, 50, 50)
+}
+
+},{"./graph":1}]},{},[2]);
