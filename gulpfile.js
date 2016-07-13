76a143ff2f9cd8f90c7bbe45beac9b089647415f
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-07-12 20:44:51 -0400

    Run gulp outside the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,43 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   browserify = require('browserify'),
-  source = require('vinyl-source-stream');
+  source = require('vinyl-source-stream'),
+  path = require('path')
 
-gulp.task('connect', function() {
+const appDir = path.join(__dirname, './app')
+const tempDir = path.join(__dirname, './tmp')
+
+function includeFile(file) {
+  return path.join(appDir, file)
+}
+
+gulp.task('connect', ['browserify', 'html'], function() {
   connect.server({
-    root: ['app', 'tmp'],
+    root: [appDir, tempDir],
     livereload: true
-  });
-});
+  })
+})
 
 gulp.task('browserify', function() {
-    return browserify('./app/main.js')
+    return browserify(includeFile('main.js'))
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./tmp/'))
-        .pipe(connect.reload());
-});
+        .pipe(gulp.dest(tempDir))
+        .pipe(connect.reload())
+})
 
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
-    .pipe(connect.reload());
-});
+  gulp.src(includeFile('*.html'))
+    .pipe(connect.reload())
+})
 
 gulp.task('watch', ['browserify', 'html'], function () {
-  gulp.watch(['./app/*.html'], ['html']);
-  gulp.watch(['./app/*.js'], ['browserify']);
-});
+  gulp.watch([includeFile('*.html')], ['html'])
+  gulp.watch([includeFile('*.js')], ['browserify'])
+})
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch'])
+
+gulp.task('serve', ['connect'])
+
+
